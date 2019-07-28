9ffa31f5a0fbca65e62c6216b40c7e87d0fc15e3
adam-weiler <adam.weiler@yahoo.ca>
2019-07-28 16:52:16 -0400

    Adding Topic class, making migrations to table, migrating changes.

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings #Needed for foreignkey.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    draft = models.BooleanField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255)
+    # topic = models.ForeignKey(
+    #     Topic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"title = {self.title}"
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=255)
+    topic = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='topics')
+
+    def __str__(self):
+        return f"topic = {self.topic}"
