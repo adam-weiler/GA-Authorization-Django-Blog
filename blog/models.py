3ebc3493ff918605667994b0d379e4933158420a
adam-weiler <adam.weiler@yahoo.ca>
2019-07-30 22:12:57 -0400

    Creating Comment class, adding to database and admin section.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
-from datetime import datetime
+# from datetime import datetime
 from django.conf import settings #Needed for foreignkey.
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ModelForm
+from django.utils import timezone # Needed for timezone.localtime()
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     draft = models.BooleanField()
-    published_date = models.DateTimeField(default=datetime.now(), blank=True)
+    published_date = models.DateTimeField(default=timezone.localtime())
     author = models.CharField(max_length=255)
     # topic = models.ForeignKey(
     #     Topic, on_delete=models.CASCADE)
@@ -21,3 +23,17 @@ class Topic(models.Model):
 
     def __str__(self):
         return f"topic = {self.topic}"
+
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.localtime())
+    message = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return f'\'{self.message}\' - {self.name}'
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'message']
