eb4a5a6c3b2474f95e2384ee5553b76efdf4ec47
adam-weiler <adam.weiler@yahoo.ca>
2019-08-05 13:31:21 -0400

    Adding a user foreignkey to the Article model.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import (MinLengthValidator,)
 
 
@@ -10,6 +11,7 @@ class Article(models.Model):
     draft = models.BooleanField(default=False)    
     published_date = models.DateField(null=True)
     author = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"'{self.title}' - by {self.author}'"
